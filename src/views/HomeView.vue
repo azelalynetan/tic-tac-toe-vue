b584bd6b9267c75b8e77e316a7ef058976fc99ff
azelalynetan <azelalynetan@gmail.com>
2025-10-31 22:54:00 +0800

    Code cleanup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Game from '@/components/Game.vue';
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="w-full md:w-2/3 mx-auto my-12 relative" id="main">
+    <h1 class="text-2xl font-bold text-center mb-4">Let's Play Tic Tac Toe!</h1>
+    <Game />
   </main>
 </template>
